db7cfe858a692613700caf45747289532979795a
soduma <soduma@naver.com>
2022-01-11 10:38:18 +0900

    Initial Commit

---
+++ Starbucks/StarbucksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarbucksApp.swift
+//  Starbucks
+//
+//  Created by 장기화 on 2022/01/11.
+//
+
+import SwiftUI
+
+@main
+struct StarbucksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
